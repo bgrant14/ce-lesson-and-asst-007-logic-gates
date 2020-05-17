65adddb5a3b71348ae4c07ea17ff7aa6d35e156f
bgrant14 <60152734+bgrant14@users.noreply.github.com>
2020-05-16 23:50:02 -0600

    Create microbit-program-4-2-6.js

---
+++ microbit-program-4-2-6.js
@@ -0,0 +1,34 @@
+//initialize variables
+let A: number = 0
+let B: number = 0
+
+basic.forever(function () {
+    B++ //iterate B
+
+    if (B > 1 && A == 1) { //A and B are 1 and 2, reset variables to zero
+        B = 0
+        A = 0
+    } else if (B > 1) { //If first part of this is not true, but B is two...
+        A++ //iterate A
+        B = 0 //set B to zero
+    }
+    //set the leds in the top left corner
+    //brightness is 0*250+5 = 5 or 1*250+5 = 255
+    led.plotBrightness(0, 0, A * 250 + 5)
+    led.plotBrightness(1, 0, B * 250 + 5)
+
+    //writes the digital outputs to pins 12 and 14
+    pins.digitalWritePin(DigitalPin.P12, A)
+    pins.digitalWritePin(DigitalPin.P14, B)
+
+    //pause to give time for gate to be set
+    basic.pause(10)
+    //reads the inputs and output their values to LEDs
+    let origPin = pins.digitalReadPin(DigitalPin.P2)
+    led.plotBrightness(2, 0, origPin * 250 + 5)
+    let equiPin = pins.digitalReadPin(DigitalPin.P1)
+    led.plotBrightness(3, 0, equiPin * 250 + 5)
+
+    //pause for a second before next loop
+    basic.pause(1000)
+})
